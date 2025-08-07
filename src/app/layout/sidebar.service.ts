d4a6782a51bf96515e5d0c7907130d2701ac70f2
Daniel <danielgilcastro@gmail.com>
2025-08-07 17:11:47 -0300

    sidebar com estilização funcional

---
+++ src/app/layout/sidebar.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+fechar() {
+throw new Error('função criada para retirar erro de compilação');
+}
+aberto: boolean = false;
+  constructor() { }
+}
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { SidebarService } from '../sidebar.service'; // ajuste o caminho se necessário
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  constructor(public sidebarService: SidebarService) { }
 }
