781c4f7a700994632fb03cfac1c69161e4268a66
Daniel <danielgilcastro@gmail.com>
2025-08-07 16:54:29 -0300

    antes de alterar no copilot

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  sideBarService = inject(SidebarService);
+
+
+  toggleideBar(){
+    this.sideBarService.toggle();
+  }
 
 }
