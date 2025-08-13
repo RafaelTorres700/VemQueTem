a071f0d7e0aeccf232b804f1ba919288762659a9
RafaelTorres700 <torres91032@gmail.com>
2025-08-13 11:41:12 -0300

    cards renderizando

---
+++ src/app/features/pol-privacidade/pol-privacidade.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pol-privacidade',
+  imports: [],
+  templateUrl: './pol-privacidade.component.html',
+  styleUrl: './pol-privacidade.component.scss'
+})
+export class PolPrivacidadeComponent {
+
+}
---
+++ src/app/features/termo-uso/termo-uso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termo-uso',
+  imports: [],
+  templateUrl: './termo-uso.component.html',
+  styleUrl: './termo-uso.component.scss'
+})
+export class TermoUsoComponent {
+
+}
